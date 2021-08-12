08416e9db29d48faa9d0ebf5c1720e7442ed1acb
Uthan Hercules <uthan.hercules12@gmail.com>
2021-08-12 09:05:11 -0300

    Listagem de empresas registradas finalizada

--- src/controllers/enterprise.js
+++ src/controllers/enterprise.js
@@ -1,7 +1,13 @@
+const knex = require('../database/connection');
+
 const createEnterprise = async (req, res) => {};
 
 // eslint-disable-next-line arrow-body-style
-const readEnterprises = async (req, res) => {};
+const readEnterprises = async (req, res) => {
+  const enterprises = await knex('enterprise');
+
+  return res.status(200).json(enterprises);
+};
 
 const updateEnterprise = async (req, res) => {};
 
--- src/database/schema.sql
+++ src/database/schema.sql
@@ -6,3 +6,22 @@ CREATE TABLE client(
   email VARCHAR(50) NOT NULL,
   password TEXT NOT NULL
 );
+
+CREATE TABLE enterprise (
+  id SERIAL NOT NULL PRIMARY KEY,
+  cnpj NUMERIC(14) NOT NULL,
+  hq BOOLEAN NOT NULL,
+  active BOOLEAN NOT NULL,
+  mei BOOLEAN NOT NULL,
+  social_name TEXT NOT NULL,
+  fantasy_name TEXT NOT NULL,
+  register_date TEXT NOT NULL,
+  cnae_number INT NOT NULL,
+  cnae_description TEXT NOT NULL,
+  secondary_cnaes JSON,
+  social_capital INT NOT NULL,
+  cep NUMERIC(8) NOT NULL,
+  address TEXT NOT NULL,
+  phone INT NOT NULL,
+  partners JSON
+);
