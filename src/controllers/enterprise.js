391290fe6a3521f48d27e372aec7dd1501d2489f
Uthan Hercules <uthan.hercules12@gmail.com>
2021-08-12 09:13:35 -0300

    Exclusão de cadastro de empresa finalizada

--- src/controllers/enterprise.js
+++ src/controllers/enterprise.js
@@ -1,17 +1,27 @@
 const knex = require('../database/connection');
+const { errors } = require('../errors/enterprise');
 
 const createEnterprise = async (req, res) => {};
 
 // eslint-disable-next-line arrow-body-style
 const readEnterprises = async (req, res) => {
   const enterprises = await knex('enterprise');
-
   return res.status(200).json(enterprises);
 };
 
 const updateEnterprise = async (req, res) => {};
 
-const deleteEnterprise = async (req, res) => {};
+const deleteEnterprise = async (req, res) => {
+  const { id } = req.params;
+  const enterprise = await knex('enterprise').select('*').where({ id });
+
+  if (enterprise.length === 0) {
+    return res.status(404).json(errors.noRegister);
+  }
+
+  await knex('enterprise').delete().where({ id });
+  return res.status(200).json('Registro deletado com sucesso!');
+};
 
 module.exports = {
   createEnterprise, readEnterprises, updateEnterprise, deleteEnterprise,
---
+++ src/errors/enterprise.js
@@ -0,0 +1,5 @@
+const errors = {
+  noRegister: 'A empresa com o ID requisitado não existe.',
+};
+
+module.exports = { errors };
